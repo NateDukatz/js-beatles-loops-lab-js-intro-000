17f818fb5f6dd34aeda61bc000acbc76b4939b70
Nate Dukatz <natedukatz@gmail.com>
2018-07-10 04:59:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicianArray, instrumentArray) {
+  var array = []
+  for(let i = 0; i < musicianArray.length; i++) {
+    array.push(`${musicianArray[i]} plays ${instrumentArray[i]}``)
+  }
+}
+
+
+
+function iLoveTheBeatles(number) {
+  var array = []
+  do {
+
+  } while()
+}
